a5df0da0d92c48c3b4698accbb23bec93c86c573
Vedant Agarwal <agarwal23vedant@gmail.com>
2026-07-18 19:26:53 +0530

    Fix session cookie for production

--- backend/server.ts
+++ backend/server.ts
@@ -29,11 +29,14 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
     store: MongoStore.create({
       mongoUrl: process.env.MONGODB_URI as string,
       collectionName: "sessions",
-    }), 
+    }),
   }),
 );
 app.use(express.json());
